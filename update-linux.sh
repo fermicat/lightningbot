ed2e947ba29fead558fdba40ddf8cfba82be5f78
quantumcatwang <zcwang.meow@gmail.com>
2018-12-10 23:12:02 -0600

    rm files

--- update-linux.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get -y upgrade
-sudo apt-get -y autoremove
-echo "\033[32m >>>>>>>>>>>>>>>>>>>> system and apps Updated to the latest version!\033[0m"
-    
-if [ -f /var/run/reboot-required ]; then
-    sudo shutdown -r +1 'The system will be restarted after 1 minutes, please re-run this script.' 
-fi
